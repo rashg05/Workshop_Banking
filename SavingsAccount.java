878c5d865c0e8acd47f04f03309fd9d50f2c9135
rashg05 <gajra.rashmi0504@gmail.com>
2022-02-22 09:19:23 +0530

    Added: Savings account workshop program

---
+++ SavingsAccount.java
@@ -0,0 +1,46 @@
+package com.bridgelab.banking;
+
+public class SavingsAccount {
+	private static double annualInterestRate;
+	
+	private double savingsBalance;
+
+	public SavingsAccount(double rate, double savingsBalance) {
+		this.annualInterestRate = rate;
+		this.savingsBalance = savingsBalance;
+	}
+	
+	public static double getAnnualInterestRate() {
+		return annualInterestRate;
+	}
+	
+	public static void setAnnualInterestRate(double annualInterestRate) {
+		SavingsAccount.annualInterestRate = annualInterestRate;
+	}
+	
+	public double getSavingsBalance() {
+		return savingsBalance;
+	}
+	
+	public void setSavingsbalance(double savingsBalance) {
+		this.savingsBalance = savingsBalance;
+	}
+	
+	public double calculateMonthlyInterest() {
+		double monthlyInterest = savingsBalance * (annualInterestRate / 100) / 12;
+		savingsBalance = savingsBalance + monthlyInterest;
+		return savingsBalance;
+	}
+	
+	
+	public void display(double interest) {
+		System.out.println("Monthly interest: " + interest);
+		System.out.println("Saving Balance: " + savingsBalance);
+	}
+	
+	public static void modifyInterestRate(double newInterestRate) {
+		annualInterestRate = newInterestRate;
+	}
+	
+	
+}
---
+++ TestSaving.java
@@ -0,0 +1,21 @@
+package com.bridgelab.banking;
+
+public class TestSaving {
+
+	public static void main(String[] args) {
+		double interest;
+		SavingsAccount saver1 = new SavingsAccount(2000, 0);
+		SavingsAccount saver2 = new SavingsAccount(3000, 0);
+		saver1.setAnnualInterestRate(4.0);
+		
+		interest = saver1.calculateMonthlyInterest();
+		
+		saver1.display(interest);
+		saver2.setAnnualInterestRate(5.0);
+		
+		interest = saver2.calculateMonthlyInterest();
+		
+		saver2.display(interest);
+	}
+
+}
