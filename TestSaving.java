fdd6df7b69d21507bdf3d01965f1a9f76ac53c29
rashg05 <gajra.rashmi0504@gmail.com>
2022-02-22 09:42:57 +0530

    Modified: Savings account workshop program

--- TestSaving.java
+++ TestSaving.java
@@ -7,14 +7,11 @@ public class TestSaving {
 		SavingsAccount saver1 = new SavingsAccount(2000.0);
 		SavingsAccount saver2 = new SavingsAccount(3000.0);
 		SavingsAccount.setAnnualInterestRate(4.0);
-		
 		interest = saver1.calculateMonthlyInterest();
-		
 		saver1.display(interest);
+		
 		SavingsAccount.setAnnualInterestRate(5.0);
-		
 		interest = saver2.calculateMonthlyInterest();
-		
 		saver2.display(interest);
 	}
 
