6bbd5c21212fabdc1f876cfd2cb51030479b8c34
rashg05 <gajra.rashmi0504@gmail.com>
2022-02-22 09:57:27 +0530

    Modified: Savings account workshop program

--- TestSaving.java
+++ TestSaving.java
@@ -3,16 +3,20 @@ package com.bridgelab.banking;
 public class TestSaving {
 
 	public static void main(String[] args) {
-		double interest;
 		SavingsAccount savingAccount1 = new SavingsAccount(2000.0);
-		SavingsAccount savingAccount2 = new SavingsAccount(3000.0);
 		SavingsAccount.setAnnualInterestRate(4.0);
-		interest = savingAccount1.calculateMonthlyInterest();
-		savingAccount1.display(interest);
+		savingAccount1.calculateMonthlyInterest();
+		System.out.println("First saver: saving balance is- " +savingAccount1.getSavingsBalance()+
+				"\nAnnual interest is- " +SavingsAccount.getAnnualInterestRate()+
+				"\nMonthly Interest- " +savingAccount1.calculateMonthlyInterest());
 		
+		SavingsAccount savingAccount2 = new SavingsAccount(3000.0);
 		SavingsAccount.setAnnualInterestRate(5.0);
-		interest = savingAccount2.calculateMonthlyInterest();
-		savingAccount2.display(interest);
+		savingAccount2.calculateMonthlyInterest();
+		System.out.println("Second saver: saving balance is- " +savingAccount2.getSavingsBalance()+
+				"\nAnnual interest is- " +SavingsAccount.getAnnualInterestRate()+
+				"\nMonthly Interest- " +savingAccount2.calculateMonthlyInterest());
+
 	}
 
 }
